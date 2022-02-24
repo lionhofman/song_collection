9972848c2fee392171a3eb9dc994130e6224041a
Lion Hofman <lion.hofman@posthaus.com.br>
2022-02-23 21:16:24 -0300

    ✨feat: day period message

--- lib/app/ui/modules/home/local_widgets/home_header.dart
+++ lib/app/ui/modules/home/local_widgets/home_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:song_collection/app/core/values/text_styles.dart';
 
 class HomeHeader extends StatelessWidget {
   const HomeHeader({Key? key}) : super(key: key);
@@ -9,22 +10,30 @@ class HomeHeader extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Text("Good Morning"),
+        Flexible(
+          child: Text(
+            dayPeriodMessage(),
+            style: TextStyles.bigTextWhiteBold,
+          ),
+        ),
         Row(
           children: [
             IconButton(
               icon: new Icon(Icons.notifications_none),
+              color: Colors.white,
               onPressed: () {
                 Get.back();
               },
             ),
             IconButton(
+              color: Colors.white,
               icon: Icon(Icons.schedule),
               onPressed: () {
                 Get.back();
               },
             ),
             IconButton(
+              color: Colors.white,
               icon: Icon(Icons.settings),
               onPressed: () {
                 Get.back();
@@ -34,5 +43,18 @@ class HomeHeader extends StatelessWidget {
         )
       ],
     );
+  }
+
+  String dayPeriodMessage() {
+    var timeNow = DateTime.now().hour;
+    if (timeNow <= 12) {
+      return 'Good Morning';
+    } else if ((timeNow > 12) && (timeNow <= 16)) {
+      return 'Good Afternoon';
+    } else if ((timeNow > 16) && (timeNow < 20)) {
+      return 'Good Evening';
+    } else {
+      return 'Good Night';
+    }
   }
 }
